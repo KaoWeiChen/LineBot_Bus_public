fbd7a0ccd2b1f7202ce061a40fdc356c1f36d647
Osalamia <qszpoi995533@gmail.com>
2022-07-05 18:53:35 +0800

    觀察event object內容v2

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def handle_message(event):
     # message = TextSendMessage(MyCommand.cmd(text, tdx_token, client_id))
 
 
-    message = TextSendMessage(str(event.message))
+    message = TextSendMessage(str(event))
     line_bot_api.reply_message(event.reply_token,message)
 
 import os
